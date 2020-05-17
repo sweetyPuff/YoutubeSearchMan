1279cbfa8785eebb1379613eff997b73536d2f49
Lucy Kuo Ting-Rung <lucy.kuo@titansoft.com.sg>
2020-05-18 00:24:31 +0800

    Use medium size img

--- src/components/VideoImage.js
+++ src/components/VideoImage.js
@@ -5,8 +5,10 @@ import { excapeSpecialCharacter } from '../helper/helper';
 export default (props) => {
     const snippet = props.video.snippet;
     return (
-        <VideoImageWrapper img={snippet.thumbnails.high.url}>
-            <div className="img-container"></div>
+        <VideoImageWrapper img={snippet.thumbnails.medium.url}>
+            <div className="img-container">
+                <img src={snippet.thumbnails.medium.url} alt=""/>
+            </div>
             <div className="title">{`${excapeSpecialCharacter(snippet.title)}`}</div>
             <div className="author">{`${excapeSpecialCharacter(snippet.channelTitle)}`}</div>
         </VideoImageWrapper>
