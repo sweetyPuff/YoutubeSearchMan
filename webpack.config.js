6ee20f55bf1f657a0f6f653456e83a1add0b7844
Lucy Kuo Ting-Rung <lucy.kuo@titansoft.com.sg>
2020-05-20 20:58:54 +0800

    Setting webpack for bundle in dev & prod env

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    context: path.resolve(__dirname, 'src'), // 切到 src 目錄下
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        // path: path.resolve(__dirname, 'App'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/,
+                use:  ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: './images/[name].[ext]?[hash]'
+                }  
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './dist',
+        watchContentBase: true
+    },
+};
